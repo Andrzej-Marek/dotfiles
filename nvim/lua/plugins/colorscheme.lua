47144d1d9bbf4ac2fbc1bc65c1e8062909aedbe7
Andrzej Marek <andrzejmarek116@gmail.com>
2025-08-27 18:19:41 +0200

    update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
- return {
+return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
@@ -8,14 +8,15 @@
   end,
 } 
 
---[[ return {
+
+ --[[ return {
   "ellisonleao/gruvbox.nvim",
   priority = 1000,
   config = function()
     vim.cmd.colorscheme("gruvbox")
   end,
-}
-]]
+} ]]
+
 --[[ return {
   {
     "folke/tokyonight.nvim",
